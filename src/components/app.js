140f17a606d0d3a2fa7bf63c99088cfff1be0ff6
Terence Highsmith <terence.highsmith@gmail.com>
2017-03-27 17:56:39 -0500

    set state

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Button from './button';
 
-const App = () => (
-  <div>
-    HI
-  </div>
-);
-
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <p>{this.state.counter}</p>
+        <Button name={'Add 1'} clicked={() => this.setState({ counter: this.state.counter++ })} />
+      </div>
+    );
+  }
+}
